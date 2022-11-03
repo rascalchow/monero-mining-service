af1aee812fb44a30c7bac199f5af2fb11b4327d2
rapiddever <supereaglehunter@gmail.com>
2022-11-03 04:29:06 -0400

    version getter and setter api added

---
+++ app/controllers/versions.js
@@ -0,0 +1,66 @@
+const Version = require('../models/version')
+const { matchedData } = require('express-validator')
+const utils = require('../middleware/utils')
+
+/********************
+ * Private functions *
+ ********************/
+
+/**
+ * Create or update a item in database
+ * @param {string} version - version string
+ */
+const upsertItem = async (version) => {
+  try {
+    return await Version.findOneAndUpdate({ version }, { version }, { upsert: true, new: true });
+  } catch (error) {
+    throw utils.buildErrObject(400, error.message)
+  }
+}
+
+/**
+ * Get most recently updated item from database
+ */
+const getLatestitem = async () => {
+  let doc = null
+  try {
+    doc = await Version.find({}, {}, { sort: { 'updatedAt' : -1 } })
+  } catch (error) {
+    throw utils.buildErrObject(500, error.message)
+  }
+  if (!doc) {
+    throw utils.buildErrObject(500, 'NO_VERSIONS_ADDED_YET')
+  }
+  return doc[0]
+}
+
+/********************
+ * Public functions *
+ ********************/
+
+/**
+ * Create version function called by route
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+exports.setCurrent = async (req, res) => {
+  req = matchedData(req)
+  try {
+    res.status(201).json(await upsertItem(req.version))
+  } catch (error) {
+    utils.handleError(res, error)
+  }
+}
+
+/**
+ * Get current version function called by route
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+ exports.getCurrent = async (req, res) => {
+  try {
+    res.status(200).json(await getLatestitem())
+  } catch (error) {
+    utils.handleError(res, error)
+  }
+}
