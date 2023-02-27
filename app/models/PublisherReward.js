4a72c4a05f72b762162138dc67bd88dee80d2721
rapiddever <supereaglehunter@gmail.com>
2023-02-27 15:53:16 +0900

    create PublisherReward model

---
+++ app/models/PublisherReward.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+
+const PublisherRewardSchema = new mongoose.Schema(
+  {
+    publisherId: {
+      type: Number,
+      required: true,
+    },
+    amount: { // newly added rev balance
+      type: Number,
+      required: true,
+    },
+    rewardBlockId: { //  id of reward block how this publisher got paid
+      type: Number,
+      required: true,
+    },
+    reason: {
+      type: String,
+      enum: ['livetime', 'master', 'affiliate'],
+      default: 'livetime',
+      required: true,
+    },
+    referralId: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true
+  }
+)
+
+PublisherRewardSchema.plugin(mongoosePaginate)
+module.exports = mongoose.model('PublisherReward', PublisherRewardSchema)
